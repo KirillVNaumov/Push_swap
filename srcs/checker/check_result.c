da9c1dbd8f14d2115a9a8d636ca59a98a5892c2e
Allen Melikian <allenwem@gmail.com>
2018-11-06 20:00:23 -0800

    LOL

--- srcs/checker/check_result.c
+++ srcs/checker/check_result.c
@@ -2,6 +2,22 @@
 
 void		check_result(t_list *stack_a, t_list *stack_b)
 {
+	int check_for_wrong;
 
-
+	if (stack_b)
+	{
+		ft_printf("error\n");
+		return ;
+	}
+	check_for_wrong = 0;
+	while (stack_a->next)
+	{
+		if (stack_a->data > stack_a->next->data)
+			check_for_wrong++;
+		stack_a = stack_a->next;
+	}
+	if (check_for_wrong > 0)
+		ft_printf("KO\n");
+	else
+		ft_printf("OK\n");
 }
