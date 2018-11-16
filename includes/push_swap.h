4e92ddc6d5fb996bf6525931c9ef229e64069bfd
Allen Melikian <allenwem@gmail.com>
2018-11-15 16:36:22 -0800

    oh

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: knaumov <knaumov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 15:28:26 by knaumov           #+#    #+#             */
-/*   Updated: 2018/11/15 13:38:31 by amelikia         ###   ########.fr       */
+/*   Updated: 2018/11/15 16:15:16 by amelikia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,19 @@ void				error_push_swap(int flag_optimized,
 int					find_solution(t_list **stack_a, int flag_v, int flag_c);
 void				find_optimal_solution(t_list *stack_a);
 int					check_answer(t_list *stack_a, t_list *stack_b);
-int					solver(t_list **stack_a, t_list **stack_b, t_comm **commands);
-int					search(t_list *stack_a, t_list *stack_b, int set_depth, t_comm **command_list);
-int					swap_rec(t_list *stack_a, t_list *stack_b, int set_depth, t_comm **command_list);
-int					push_rec(t_list *stack_a, t_list *stack_b, int set_depth, t_comm **command_list);
-int					rotate_rec(t_list *stack_a, t_list *stack_b, int set_depth, t_comm **command_list);
-int					reverse_rotate_rec(t_list *stack_a, t_list *stack_b, int set_depth, t_comm **command_list);
-int         		check_last_comm(t_comm *list, char *command);
+int					solver(t_list **stack_a, t_list **stack_b,
+	t_comm **commands);
+int					search(t_list *stack_a, t_list *stack_b,
+	int set_depth, t_comm **command_list);
+int					swap_rec(t_list *stack_a, t_list *stack_b,
+	int set_depth, t_comm **command_list);
+int					push_rec(t_list *stack_a, t_list *stack_b,
+	int set_depth, t_comm **command_list);
+int					rotate_rec(t_list *stack_a, t_list *stack_b,
+	int set_depth, t_comm **command_list);
+int					reverse_rotate_rec(t_list *stack_a, t_list *stack_b,
+	int set_depth, t_comm **command_list);
+int					check_last_comm(t_comm *list, char *command);
+t_comm			*optimize_answer(t_comm *commands);
 
 #endif
--- srcs/push_swap/optimize_answer.c
+++ srcs/push_swap/optimize_answer.c
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   find_solution.c                                    :+:      :+:    :+:   */
+/*   optimize_answer.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amelikia <amelikia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/10 14:42:12 by amelikia          #+#    #+#             */
-/*   Updated: 2018/11/15 16:34:09 by amelikia         ###   ########.fr       */
+/*   Created: 2018/11/15 15:27:01 by amelikia          #+#    #+#             */
+/*   Updated: 2018/11/15 16:31:40 by amelikia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	check_for_right_commnds(t_comm *commands)
+t_comm	*optimize_answer(t_comm *commands)
 {
+	t_comm	*word;
+
+	word = NULL;
 	while (commands->next)
 	{
 		if ((!ft_strcmp(commands->command, "ra") &&
@@ -27,42 +30,33 @@ int	check_for_right_commnds(t_comm *commands)
 		(!ft_strcmp(commands->command, "rb") &&
 		!ft_strcmp(commands->next->command, "rrb"))
 		|| (!ft_strcmp(commands->command, "rrb") &&
-		!ft_strcmp(commands->next->command, "rb")) ||
-		(!ft_strcmp(commands->command, "rra") &&
-		!ft_strcmp(commands->next->command, "rrb")) ||
-		(!ft_strcmp(commands->command, "rrb")
+		!ft_strcmp(commands->next->command, "rb")))
+		{
+			if (commands->next)
+				if (commands->next->next)
+				{
+					commands = commands->next;
+					commands = commands->next;
+				}
+			continue;
+		}
+		else if ((!ft_strcmp(commands->command, "rra") &&
+		!ft_strcmp(commands->next->command, "rrb"))
+		|| (!ft_strcmp(commands->command, "rrb")
 		&& !ft_strcmp(commands->next->command, "rra")))
-			return (0);
+		{
+			if (commands->next)
+				if (commands->next->next)
+				{
+					commands = commands->next;
+					commands = commands->next;
+				}
+			word = ft_comm_add_back(word, "rrr");
+		}
+		else
+			word = ft_comm_add_back(word, commands->command);
 		commands = commands->next;
 	}
-	return (1);
-}
-
-int	find_solution(t_list **stack_a, int flag_v, int flag_c)
-{
-	t_list	*stack_b;
-	t_comm	*commands;
-	int		stop;
-
-	stop = 0;
-	commands = NULL;
-	stack_b = NULL;
-	*stack_a = list_assign_pos(*stack_a);
-	while (1)
-	{
-		if (solver(stack_a, &stack_b, &commands) == 1)
-			stop = 1;
-		if (flag_v == 1)
-			print_status(*stack_a, stack_b, flag_c);
-		if (stop == 1)
-			break ;
-	}
-	while (check_for_right_commnds(commands) == 0)
-		commands = optimize_answer(commands);
-	while (commands)
-	{
-		ft_printf("%s\n", commands->command);
-		commands = commands->next;
-	}
-	return (0);
+	word = ft_comm_add_back(word, commands->command);
+	return (word);
 }
